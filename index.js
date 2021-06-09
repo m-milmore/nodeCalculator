d289bd0f8bf3b23f8ad315654e5e186346f9168c
m-milmore <m.milmore@mmci.qc.ca>
2021-06-09 19:44:55 -0400

    2nd commit

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 const rs = require("readline-sync");
 
-var operation = "";
-var operator = "";
-var operand1 = 0;
-var operand2 = 0;
-var result = "";
-
 function getOperation() {
     let operation = rs.question("What operation would you like to perform? ", {
-        limit: [/^\d*\s*[-|\/|\*|\+]\s*\d*$/g],
-        limitMessage: "That is not a valid operation. $<( [)limit(])>",
+        limit: [/^(-|\/|\*|\+){1}$|^-?\d+\s*[-|\/|\*|\+]\s*-?\d+$/g],
+        limitMessage: "That is not a valid operation.",
     });
     return operation.replace(/ /g, "");
 }
 
-function divisionByZero(operand = "first", operator = "+", value = 1) {
-    return operand === "second" && operator === "/" && value === 0;
-}
-
-function getNumber(operand = "first", operator = "+") {
-    let acceptZero = false;
-    let number = 0;
-    while (!acceptZero) {
-        number = rs.questionInt(`Please enter the ${operand} number `, {
-            limitMessage: "This is not a number",
-        });
-        if (divisionByZero(operand, operator, number)) {
-            console.log("Division by 0 not allowed.");
-        } else {
-            acceptZero = true;
-        }
-    }
+function getNumber(operand = "") {
+    number = rs.questionInt(`Please enter the ${operand} number `, {
+        limitMessage: "This is not a number",
+    });
     return number;
 }
 
-function calculateOperation(operator = "", operand1 = 0, operand2 = 0) {
+function OperationObj(operation) {
+    let position = 0;
+    if (operation.length === 1) {
+        this.operator = operation;
+        this.operand1 = getNumber("first");
+        this.operand2 = getNumber("second");
+    } else {
+        if (operation.charAt(0) === "-") {
+            const afterNegativeSign = operation.substr(1);
+            position = afterNegativeSign.search(/[-|\/|\*|\+]/g) + 1;
+        } else {
+            position = operation.search(/[-|\/|\*|\+]/g);
+        }
+        this.operator = operation[position];
+        this.operand1 = parseInt(operation.substr(0, position));
+        this.operand2 = parseInt(operation.substr(position + 1));
+    }
+}
+
+function calculateOperation({ operator, operand1, operand2 }) {
     let result = 0;
     let remainder = 0;
     switch (operator) {
@@ -56,41 +56,12 @@ function calculateOperation(operator = "", operand1 = 0, operand2 = 0) {
     }
 
     if (operator === "/" && operand2 !== 0) {
-        return (`The result is: ${result} remains ${remainder}`);
+        return `The result is: ${result} remains ${remainder}`;
     } else {
-        return (`The result is: ${result}`);
+        return `The result is: ${result}`;
     }
 }
 
-function getFromString(str = "", operand = "first", operator = "+") {
-    let number = parseInt(str);
-    if (divisionByZero(operand, operator, number)) {
-        console.log("Division by 0 not allowed.");
-        number = getNumber("second", operator);
-    }
-    return number;
-}
-
-operation = getOperation();
-if (operation.length === 1) {
-    operator = operation;
-    operand1 = getNumber();
-    operand2 = getNumber("second", operator);
-} else {
-    if ("/*-+".includes(operation.charAt(0))) {
-        operator = operation.charAt(0);
-        operand1 = getNumber();
-        operand2 = getFromString(operation.substr(1), "second", operator);
-    } else if ("/*-+".includes(operation.charAt(operation.length - 1))) {
-        operator = operation.charAt(operation.length - 1);
-        operand1 = getFromString(operation.substr(0, operation.length - 1));
-        operand2 = getNumber("second", operator);
-    } else {
-        const position = operation.search(/[-|\/|\*|\+]/g);
-        operator = operation[position];
-        operand1 = parseInt(operation.substr(0, position));
-        operand2 = parseInt(operation.substr(position + 1));
-    }
-}
-result = calculateOperation(operator, operand1, operand2);
-console.log(result);
+const operation = getOperation();
+const operationObj = new OperationObj(operation);
+console.log(calculateOperation(operationObj));
--- test.js
+++ test.js
@@ -1,10 +1,48 @@
 const rs = require("readline-sync");
 
-const operation = rs.question("What operation would you like to perform? ", {
-    limit: [/^\d*\s*[-|\/|\*|\+]\s*\d*$/g],
-    limitMessage: "That is not a valid operation. $<( [)limit(])>",
-});
+function getOperation() {
+    let operation = rs.question("What operation would you like to perform? ", {
+        limit: [/^-?\d*\s*[-|\/|\*|\+]\s*-?\d*$/g],
+        limitMessage: "That is not a valid operation.",
+    });
+    return operation.replace(/ /g, "");
+}
 
+function getNumber(operand = "") {
+    number = rs.questionInt(`Please enter the ${operand} number `, {
+        limitMessage: "This is not a number",
+    });
+    return number;
+}
 
+function OperationObj(operation) {
 
-console.log(operation);
+    arrayOp = operation.split();
+    arrayOp.forEach(ele => {
+
+    });
+
+    if (operation.length === 1) {
+        this.operator = operation;
+        this.operand1 = getNumber("first");
+        this.operand2 = getNumber("second");
+    } else {
+        if ("/*-+".includes(operation.charAt(0))) {
+            this.operator = operation.charAt(0);
+            this.operand1 = getNumber("first");
+            this.operand2 = parseInt(operation.substr(1));
+        } else if ("/*-+".includes(operation.charAt(operation.length - 1))) {
+            this.operator = operation.charAt(operation.length - 1);
+            this.operand1 = parseInt(operation.substr(0, operation.length - 1));
+            this.operand2 = getNumber("second");
+        } else {
+            const position = operation.search(/[-|\/|\*|\+]/g);
+            this.operator = operation[position];
+            this.operand1 = parseInt(operation.substr(0, position));
+            this.operand2 = parseInt(operation.substr(position + 1));
+        }
+    }
+}
+
+const operation = getOperation();
+const operationObj = new OperationObj(operation);
